2aecc48e9fd38ca4cbb6b06e64564e7570872a39
Antoniotravisani <98852884+Antoniotravisani@users.noreply.github.com>
2023-12-03 13:38:43 -0300

    Update alldata.js tryn to fix json problem

--- public/alldata.js
+++ public/alldata.js
@@ -4,11 +4,19 @@ function AllData(){
     React.useEffect(() => {
         
         // fetch all accounts from API
-        fetch('/badbank1-a1794b6d5c62.herokuapp.com/account/all ')
-            .then(response => response.json())
+        fetch('/badbank1-a1794b6d5c62.herokuapp.com/account/all')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
             .then(data => {
                 console.log(data);
                 setData(JSON.stringify(data));                
+            })
+            .catch(error => {
+                console.error('Error:', error);
             });
 
     }, []);
