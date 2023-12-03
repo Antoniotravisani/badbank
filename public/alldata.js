13a679661fc615fb21c0b30813be5c80670fdbd4
Antoniotravisani <98852884+Antoniotravisani@users.noreply.github.com>
2023-12-03 16:17:36 -0300

    Update alldata.js

--- public/alldata.js
+++ public/alldata.js
@@ -4,19 +4,11 @@ function AllData(){
     React.useEffect(() => {
         
         // fetch all accounts from API
-        fetch('/account/all')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
+        fetch('/account/all ')
+            .then(response => response.json())
             .then(data => {
                 console.log(data);
                 setData(JSON.stringify(data));                
-            })
-            .catch(error => {
-                console.error('Error:', error);
             });
 
     }, []);
