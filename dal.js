bd159bd05646769c1073e239d9cc8f6e31040f10
Antoniotravisani <98852884+Antoniotravisani@users.noreply.github.com>
2023-12-03 10:00:46 -0300

    Update dal.js para asynchronous

--- dal.js
+++ dal.js
@@ -1,94 +1,100 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
-const uri         = 'mongodb+srv://antoniotravisani16:Aa00151200@cluster1.ontlmng.mongodb.net/?retryWrites=true&w=majority';
-let db            = null;
- 
-// connect to mongo
-MongoClient.connect(uri, {useUnifiedTopology: true}, function(err, client) {
-    console.log("Connected successfully to db server");
+const url = 'mongodb://localhost:27017';
+const uri = 'mongodb+srv://antoniotravisani16:Aa00151200@cluster1.ontlmng.mongodb.net/?retryWrites=true&w=majority';
+let db = null;
 
-    // connect to myproject database
-    db = client.db('BBankdatabase');
-});
+// Function to connect to MongoDB
+function connectToDB() {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, client) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Connected successfully to db server");
+                db = client.db('BBankdatabase');
+                resolve();
+            }
+        });
+    });
+}
 
-// create user account
-function create(name, email, password){
-    return new Promise((resolve, reject) => {    
-        const collection = db.collection('users');
-        const doc = {name, email, password, balance: 0};
-        collection.insertOne(doc, {w:1}, function(err, result) {
-            err ? reject(err) : resolve(doc);
-        });    
+// Ensure the connection is established before exporting the functions
+connectToDB()
+    .then(() => {
+        // Functions to interact with the database
+        function create(name, email, password) {
+            return new Promise((resolve, reject) => {
+                const collection = db.collection('users');
+                const doc = { name, email, password, balance: 0 };
+                collection.insertOne(doc, { w: 1 }, function (err, result) {
+                    err ? reject(err) : resolve(doc);
+                });
+            });
+        }
+
+        function find(email) {
+            return new Promise((resolve, reject) => {
+                const customers = db
+                    .collection('users')
+                    .find({ email: email })
+                    .toArray(function (err, docs) {
+                        err ? reject(err) : resolve(docs);
+                    });
+            });
+        }
+
+        function findOne(email) {
+            return new Promise((resolve, reject) => {
+                const customers = db
+                    .collection('users')
+                    .findOne({ email: email })
+                    .then((doc) => resolve(doc))
+                    .catch((err) => reject(err));
+            });
+        }
+
+        function update(email, amount) {
+            return new Promise((resolve, reject) => {
+                const customers = db
+                    .collection('users')
+                    .findOneAndUpdate(
+                        { email: email },
+                        { $inc: { balance: amount } },
+                        { returnOriginal: false },
+                        function (err, documents) {
+                            err ? reject(err) : resolve(documents);
+                        }
+                    );
+            });
+        }
+
+        function all() {
+            return new Promise((resolve, reject) => {
+                const customers = db
+                    .collection('users')
+                    .find({})
+                    .toArray(function (err, docs) {
+                        err ? reject(err) : resolve(docs);
+                    });
+            });
+        }
+
+        function balance(email) {
+            return new Promise((resolve, reject) => {
+                const customers = db
+                    .collection('users')
+                    .find({ email: email })
+                    .toArray()
+                    .then((docs) => {
+                        console.log(docs);
+                        resolve(docs);
+                    })
+                    .catch((err) => reject(err));
+            });
+        }
+
+        module.exports = { create, findOne, find, update, all, balance };
     })
-}
-
-// find user account
-function find(email){
-    return new Promise((resolve, reject) => {    
-        const customers = db
-            .collection('users')
-            .find({email: email})
-            .toArray(function(err, docs) {
-                err ? reject(err) : resolve(docs);
-        });    
-    })
-}
-
-// find user account
-function findOne(email){
-    return new Promise((resolve, reject) => {    
-        const customers = db
-            .collection('users')
-            .findOne({email: email})
-            .then((doc) => resolve(doc))
-            .catch((err) => reject(err));    
-    })
-}
-
-// update - deposit/withdraw amount
-function update(email, amount){
-    return new Promise((resolve, reject) => {    
-        const customers = db
-            .collection('users')            
-            .findOneAndUpdate(
-                {email: email},
-                { $inc: { balance: amount}},
-                { returnOriginal: false },
-                function (err, documents) {
-                    err ? reject(err) : resolve(documents);
-                }
-            );            
-
-
-    });    
-}
-
-// all users
-function all(){
-    return new Promise((resolve, reject) => {    
-        const customers = db
-            .collection('users')
-            .find({})
-            .toArray(function(err, docs) {
-                err ? reject(err) : resolve(docs);
-        });    
-    })
-}
-
-// balance EU CRIEI
-function balance(email){
-    return new Promise((resolve, reject) => {    
-        const customers = db
-            .collection('users')
-            .find({email: email})
-            .toArray()
-            .then((docs) => {
-                console.log(docs);
-                resolve(docs);
-            })
-            .catch((err) => reject(err)); 
-    });    
-}
-
-
-module.exports = {create, findOne, find, update, all, balance};
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
