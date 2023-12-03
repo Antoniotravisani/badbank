612f90ef3c1b96cbf85266af62f1b43718be2816
Antoniotravisani <98852884+Antoniotravisani@users.noreply.github.com>
2023-12-03 10:24:31 -0300

    Update alldata.js get - http[heroku]/account/all

--- public/alldata.js
+++ public/alldata.js
@@ -4,7 +4,7 @@ function AllData(){
     React.useEffect(() => {
         
         // fetch all accounts from API
-        fetch('/account/all')
+        fetch('/badbank1-a1794b6d5c62.herokuapp.com/account/all ')
             .then(response => response.json())
             .then(data => {
                 console.log(data);
